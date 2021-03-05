a20b97024092aec5888d24ea9c12d3ffcf57db4c
Kacper Salek <kacper.salek32@gmail.com>
2021-03-05 22:43:08 +0100

    handle users routes

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=>{
-	res.send('hello world!');
+const users = [
+  {
+	  id: 1,
+    name: "kacper",
+    age: 22,
+  },
+  {
+	  id: 2,
+    name: "John",
+    age: 33,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+app.get("/users/:id", (req, res) => {
+  const id = 
 });
 
-app.listen(port, ()=>console.log(`Example app listen on port: ${port}`));
+app.listen(port, () => console.log(`Example app listen on port: ${port}`));
