5e21891529306ba64d041239687a650da26469e8
Kacper Salek <kacper.salek32@gmail.com>
2021-03-06 12:10:49 +0100

    my first custom header

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const port = 3000;
 
 const users = [
   {
-	  id: 1,
+    id: 1,
     name: "kacper",
     age: 22,
   },
   {
-	  id: 2,
+    id: 2,
     name: "John",
     age: 33,
   },
@@ -18,11 +18,11 @@ const users = [
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-app.get("/users/:id", (req, res) => {
-  const id = 
+
+app.get("/test", (req, res) => {
+  res.set("X-my-custom-header", "example text");
+  res.status(418);
+  res.send("i dont know");
 });
 
 app.listen(port, () => console.log(`Example app listen on port: ${port}`));
