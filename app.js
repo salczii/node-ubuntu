36f542c514a458d82c969ebb1a625a8e6be45ce3
Kacper Salek <kacper.salek32@gmail.com>
2021-03-08 23:49:12 +0100

    add ui part of app

--- app.js
+++ app.js
@@ -65,4 +65,10 @@ wss.on("connection", function connection(ws) {
   });
 });
 
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
 app.listen(port, () => console.log(` app listen on port: ${port}`));
